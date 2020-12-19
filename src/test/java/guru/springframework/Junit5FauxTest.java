3e2a3ab142dc4fe6ee5419ae52010e59d06e8327
arunabhamidipati <arunprasad_bh@yahoo.com>
2020-12-19 11:49:25 +0000

    Committing the Junit4 and Junit5 tests for Junit4 we need Junit dependency for Junit5 we need Jupitar Api, engine, and vintage engine dependencies. Surefire plugin 2.22.0 is also needed.

---
+++ src/test/java/guru/springframework/Junit5FauxTest.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5FauxTest {
+    @Test
+    void someTestForJunit5() {
+        System.out.println("I ran ...!");
+    }
+}

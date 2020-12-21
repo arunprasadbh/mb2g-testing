089f14ca726ac9bb25b00760c3de76a868efec8c
arunabhamidipati <arunprasad_bh@yahoo.com>
2020-12-21 18:21:23 +0000

    Modified the pom. Added failsafe plugin and a integration test. Added the reporting plugin maven-surefire-report-plugin
    Final commit for this branch.

---
+++ src/test/java/guru/springframework/JavaHelloWorld2Test.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaHelloWorld2Test {
+    @Test
+    void getHello() {
+        System.out.println("My surefire test 2");
+    }
+}
---
+++ src/test/java/guru/springframework/JavaHelloWorldTestIT.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaHelloWorldTestIT {
+
+    @Test
+    void getHello() {
+        System.out.println("My Integration Tests worked");
+    }
+}
